cb26d15b320ca7fcff3f16f8480e6e27776de9f2
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-12-05 19:13:49 +0100

    Add PropertyGrid control

---
+++ src/TemplateMAUI/Controls/PropertyGrid/Editors/TimePropertyEditor.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TemplateMAUI.Controls
+{
+    internal class TimePropertyEditor : PropertyEditorBase
+    {
+        public override View CreateView(PropertyItem propertyItem) => new TimePicker(); 
+        
+        public override BindableProperty GetBindableProperty() => TimePicker.TimeProperty;
+    }
+}
---
+++ src/TemplateMAUI/Extensions/EnumerableExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace TemplateMAUI.Extensions
+{
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<TSource> Invoke<TSource>(this IEnumerable<TSource> source, Action<TSource> predicate)
+        {
+            var enumerable = source as IList<TSource> ?? source.ToList();
+            foreach (var item in enumerable)
+            {
+                predicate.Invoke(item);
+            }
+
+            return enumerable;
+        }
+    }
+}

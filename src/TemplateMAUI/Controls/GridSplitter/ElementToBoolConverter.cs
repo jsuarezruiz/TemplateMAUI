7d5cfabed5cf08cfee20c44a5506a429a5aa49a5
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-01-06 18:09:36 +0100

    Fix GridSplitter wrong indicator rendering

--- src/TemplateMAUI/Controls/GridSplitter/ElementToBoolConverter.cs
+++ src/TemplateMAUI/Controls/GridSplitter/ElementToBoolConverter.cs
@@ -6,7 +6,7 @@ namespace TemplateMAUI.Controls
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null;
+            return value is null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

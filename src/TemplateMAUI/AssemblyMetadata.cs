e3bb52829f59a93345678d7f09f2ef0bffab46f9
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-02-01 13:22:59 +0100

    Added custom namespace schemas

---
+++ src/TemplateMAUI/AssemblyMetadata.cs
@@ -0,0 +1,7 @@
+﻿using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
+
+// Custom xaml schema
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/2021/maui/templatemaui", "TemplateMAUI.Controls")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/2021/maui/templatemaui/datavisualization", "TemplateMAUI.DataVisualization")]
+// Recommended prefix
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/dotnet/2021/maui/templatemaui", "tmaui")]
